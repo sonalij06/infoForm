64e150e8b8eee724f0a2c6f4fb5704147786f428
SonaliJain <sonali.jain.cs.2018@miet.ac.in>
2023-08-26 04:19:17 +0530

    testing github action_16

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 try {
   // MySQL connection code here
   const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: 'localhost',
+    user: 'root',
+    password: 'Sonali@123',
+    database: 'web_page'
   });
 
 
